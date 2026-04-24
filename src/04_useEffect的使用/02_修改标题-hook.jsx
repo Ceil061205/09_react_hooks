7a1eeeeac9d3651f3a450f44c43b2b344e222430
Raven <raven@yiqicai.com>
2026-04-24 17:45:59 +0800

    feat: useEffect

--- src/04_useEffect的使用/02_修改标题-hook.jsx
+++ src/04_useEffect的使用/02_修改标题-hook.jsx
@@ -3,7 +3,9 @@ import React, { memo, useState, useEffect } from 'react'
 const App = memo(() => {
   const [count, setCount] = useState(0)
 
+  // 放一些额外副作用的代码 组件渲染完成后执行
   useEffect(() => {
+    // 网络请求 dom操作 事件监听
     // 组件每次渲染完成后自动执行
     document.title = `你点击了${count}次`
   })
